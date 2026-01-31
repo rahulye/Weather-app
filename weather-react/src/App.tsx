0cc14cade3b4105bc4c7f6a11c219c3945c9a051
Rahul-krishh <rahulmykris@gmail.com>
2026-01-31 23:22:56 +0530

    feat : initiate react

---
+++ weather-react/src/App.tsx
@@ -0,0 +1,14 @@
+/** @format */
+import "./App.css";
+import type { FC } from "react";
+import { MainPage } from "./components/MainPage";
+
+const App:FC = () => {
+	return (
+    <>
+      <MainPage />
+    </>
+  );
+}
+
+export default App;
